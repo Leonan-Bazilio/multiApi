7fe1cdcbc91d3c57d24ff301645a6b6e497711ae
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2024-07-26 18:44:38 -0300

    api de buscar filmes basica adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import LanguageTranslator from "./components/LanguageTranlator";
+import MovieSearch from "./components/MovieSearch";
 
 function App() {
   return (
     <>
       <LanguageTranslator />
+      <MovieSearch />
     </>
   );
 }
---
+++ src/components/MovieSearch/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+import styles from "./styles.module.css";
+
+export default function MovieSearch() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async () => {
+    try {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=${query}&apikey=403abbfe`
+      );
+      setMovies(response.data.Search);
+    } catch (error) {
+      console.error("Error fetching movie data:", error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Movie Search Engine</h1>
+      <input className="query"
+        type="text"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Search for a movie"
+      />
+      <button className="search" onClick={searchMovies}>Search</button>
+      <div className={styles.moviesContainer}>
+        {movies &&
+          movies.map((movie) => (
+            <div className={styles.movieCard} key={movie.imdbID}>
+              <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
